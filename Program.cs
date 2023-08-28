cc3b4d47a57a6be4d3553c414ec98e828f4ef0f9
Henrique Bueno Foseca <108840429+HenriBueno00@users.noreply.github.com>
2023-08-28 20:39:21 -0300

    Aula 1 - 24/08

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1_VBNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = 20, b = 10;
+            Console.WriteLine("Hello World!!");
+            Console.WriteLine("Fatec Itapetininga");
+            Console.WriteLine("Analise e desenvolvimento de sistemas");
+
+            Console.WriteLine(A+" + "+b+" = "+(A+b));
+            Console.WriteLine(A + " x " + b + " = " + (A * b));
+            Console.WriteLine(A + " mod " + b + " = " + (A % b));
+            Console.WriteLine("{0} + {1} = {2:N2
+                }",A,b,(A+b));
+
+            Console.ReadKey();
+        }
+    }
+}
