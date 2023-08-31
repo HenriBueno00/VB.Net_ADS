9df3f36ab4bab43d927393e86746310cd75c4edd
Henrique Bueno Foseca <108840429+HenriBueno00@users.noreply.github.com>
2023-08-31 19:44:18 -0300

    Aula 31/08 -  OPERAÇÕES DENTRO DE C#

--- Program.cs
+++ Program.cs
@@ -4,34 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Aula_31_08_Projeto3
+namespace Aula_31_08_Operacoes
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string cidade, nome;
-            int idade;
-            double peso;
+            int a, b, c;
 
-            Console.Write("Informe seu nome: ");
-            nome = Console.ReadLine();
+            Console.Write("Informe o valor de A: ");
+            a = int.Parse(Console.ReadLine());
 
-            Console.Write("Informe sua cidade: ");
-            cidade = Console.ReadLine();
+            Console.Write("Informe o valor de B: ");
+            b = int.Parse(Console.ReadLine());
 
-            Console.Write("Informe sua idade: ");
-            idade = int.Parse(Console.ReadLine());
+            Console.Write("Informe o valor de C: ");
+            c = int.Parse(Console.ReadLine());
 
-            Console.Write("Informe seu peso: ");
-            peso = double.Parse(Console.ReadLine());
+            c = a + b;
 
-            Console.WriteLine("Informações Digitadas:\n");
-            Console.WriteLine("Seu nome é {0}", nome);
-            Console.WriteLine("Sua ciade é {0}", cidade);
-            Console.WriteLine("Sua idade é {0} Anos", idade);
-            Console.WriteLine("Seu peso é {0} Kg", peso);
-
+            Console.Write(a + " + " + b + " + " + c);
             Console.ReadKey();
         }
     }
