6da5eb74b918b4e73beec085ecaebe4dfc13647d
Henrique Bueno Foseca <108840429+HenriBueno00@users.noreply.github.com>
2023-08-31 19:39:09 -0300

    Aula - 31/08 - ENTRADA DE DADOS EM C#

--- Program.cs
+++ Program.cs
@@ -4,22 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Aula1_VBNet
+namespace Aula_31_08_Projeto3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double A = 20, b = 10;
-            Console.WriteLine("Hello World!!");
-            Console.WriteLine("Fatec Itapetininga");
-            Console.WriteLine("Analise e desenvolvimento de sistemas");
+            string cidade, nome;
+            int idade;
+            double peso;
 
-            Console.WriteLine(A+" + "+b+" = "+(A+b));
-            Console.WriteLine(A + " x " + b + " = " + (A * b));
-            Console.WriteLine(A + " mod " + b + " = " + (A % b));
-            Console.WriteLine("{0} + {1} = {2:N2
-                }",A,b,(A+b));
+            Console.Write("Informe seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Informe sua cidade: ");
+            cidade = Console.ReadLine();
+
+            Console.Write("Informe sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe seu peso: ");
+            peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informações Digitadas:\n");
+            Console.WriteLine("Seu nome é {0}", nome);
+            Console.WriteLine("Sua ciade é {0}", cidade);
+            Console.WriteLine("Sua idade é {0} Anos", idade);
+            Console.WriteLine("Seu peso é {0} Kg", peso);
 
             Console.ReadKey();
         }
